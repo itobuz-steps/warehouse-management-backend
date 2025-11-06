6638b13943d11d8b9c2101abed9b34b0714571a5
Devjyoti Banerjee <devjyoti@itobuz.com>
2025-11-06 17:08:23 +0530

    fix: stock-in transaction controller

--- src/controllers/TransactionController.js
+++ src/controllers/TransactionController.js
@@ -1,6 +1,8 @@
 import Transaction from '../models/transactionModel.js';
 import tokenValidator from '../utils/verifyToken.js';
 import Product from '../models/productModel.js';
+import Warehouse from '../models/warehouseModel.js';
+import Quantity from '../models/quantityModel.js';
 import mongoose from 'mongoose';
 
 export default class TransactionController {
@@ -17,41 +19,78 @@ export default class TransactionController {
   };
 
   createStockIn = async (req, res, next) => {
+    const session = await mongoose.startSession();
+    session.startTransaction();
+
     try {
       const bearer_token = req.headers.authorization;
       const access_token = bearer_token.split(' ')[1];
-      const { product, quantity, supplier, notes } = req.body;
+      const { products, supplier, notes, destinationWarehouse } = req.body;
 
       const userId = await tokenValidator(access_token);
-      console.log(userId._id.toString());
+      // console.log(userId._id.toString());
 
-      let productId = product;
+      const warehouse = await Warehouse.findOne({ name: destinationWarehouse });
 
-      if (!mongoose.Types.ObjectId.isValid(product)) {
-        const foundProduct = await Product.findOne({ name: product });
-
-        foundProduct.quantity += quantity;
-        await foundProduct.save();
-
-        if (!foundProduct) {
-          return res.status(400).json({ message: 'Product not found' });
-        }
-
-        productId = foundProduct._id;
+      if (!warehouse) {
+        return res
+          .status(400)
+          .json({ message: 'Destination warehouse not found' });
       }
 
-      const transaction = new Transaction({
-        type: 'IN',
-        product: productId,
-        quantity,
-        supplier,
-        notes,
-        performedBy: userId._id,
-      });
+      const transactions = [];
 
-      const createdTransaction = await transaction.save();
-      res.status(201).json(createdTransaction);
+      for (const item of products) {
+        const { name, quantity } = item;
+
+        const product = await Product.findOne({ name });
+
+        if (!product) {
+          await session.abortTransaction();
+          return res.status(400).json({ message: `Product ${name} not found` });
+        }
+
+        let quantityRecord = await Quantity.findOne({
+          warehouseId: warehouse._id,
+          productId: product._id,
+        });
+
+        if (quantityRecord) {
+          quantityRecord.quantity += quantity;
+        } else {
+          quantityRecord = new Quantity({
+            warehouseId: warehouse._id,
+            productId: product._id,
+            quantity,
+          });
+        }
+
+        await quantityRecord.save({ session });
+
+        const transaction = new Transaction({
+          type: 'IN',
+          product: product._id,
+          quantity,
+          supplier,
+          destinationWarehouse: warehouse._id,
+          notes,
+          performedBy: userId._id,
+        });
+
+        const createdTransaction = await transaction.save({ session });
+        transactions.push(createdTransaction);
+      }
+
+      await session.commitTransaction();
+      session.endSession();
+
+      res.status(201).json({
+        message: 'Stock-in transactions created successfully',
+        transactions,
+      });
     } catch (error) {
+      await session.abortTransaction();
+      session.endSession();
       next(error);
     }
   };
