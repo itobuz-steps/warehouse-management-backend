822a2b7655d8f5f9deeb6e53b4073de1e08a19b7
Sanket Ganguly <sanket@itobuz.com>
2025-11-14 12:41:46 +0530

    WMDT-73 feat: modular functionality of pdf generation

--- src/services/generatePdf.js
+++ src/services/generatePdf.js
@@ -11,33 +11,25 @@ const generatePdf = async (transaction) => {
   const boldFont = await pdfDoc.embedFont(StandardFonts.HelveticaBold);
   const height = page.getSize().height;
 
+  // Get the image file path
   const __filename = fileURLToPath(import.meta.url);
   const __dirname = dirname(__filename);
-
   const imagePath = path.join(
     __dirname,
     '../assets/images/warehouse-logo-removebg-preview.png'
   );
+
+  // Get the image and set the height and width
   const imageBytes = await fs.readFile(imagePath);
   const pngImage = await pdfDoc.embedPng(imageBytes);
   const imgWidth = 120;
   const imgHeight = (pngImage.height / pngImage.width) * imgWidth;
 
   // Draw the image on the page
-  page.drawImage(pngImage, {
-    x: 100,
-    y: 710,
-    width: imgWidth,
-    height: imgHeight,
-  });
+  drawImage(page, pngImage, 100, 710, imgWidth, imgHeight);
 
-  page.drawImage(pngImage, {
-    x: 150,
-    y: 250,
-    width: 300,
-    height: 300,
-    opacity: 0.2,
-  });
+  // Draw background logo
+  drawImage(page, pngImage, 150, 250, 300, 300, 0.2);
 
   const {
     product,
@@ -51,208 +43,172 @@ const generatePdf = async (transaction) => {
     _id,
   } = transaction;
 
+  // Set line gap and Y axis length
   const lineGap = 20;
   let cursorY = height - 80;
 
-  // === HEADER ===
-  page.drawText('STOCK OUT INVOICE', {
-    x: 200,
-    y: cursorY,
-    size: 24,
-    font: boldFont,
-    color: rgb(0.725, 0.478, 0.529),
-  });
+  // ---------------- HEADER ----------------
+  writeText(
+    page,
+    'STOCK OUT INVOICE',
+    200,
+    cursorY,
+    24,
+    boldFont,
+    rgb(0.725, 0.478, 0.529)
+  );
 
   cursorY -= 40;
-  page.drawText(`Transaction ID: `, {
-    x: 50,
-    y: cursorY,
-    size: 12,
+  writeText(
+    page,
+    `Transaction ID: `,
+    50,
+    cursorY,
+    12,
     font,
-    color: rgb(0.725, 0.478, 0.529),
-  });
-  page.drawText(`${_id}`, {
-    x: 140,
-    y: cursorY,
-    size: 12,
-    font: boldFont,
-  });
+    rgb(0.725, 0.478, 0.529)
+  );
+  writeText(page, `${_id}`, 140, cursorY, 12, boldFont);
 
   cursorY -= lineGap;
-  page.drawLine({
-    start: { x: 50, y: cursorY },
-    end: { x: 550, y: cursorY },
-    thickness: 1,
-    color: rgb(0.725, 0.478, 0.529),
-  });
+  writeLine(
+    page,
+    { x: 50, y: cursorY },
+    { x: 550, y: cursorY },
+    1,
+    rgb(0.725, 0.478, 0.529)
+  );
 
   cursorY -= lineGap * 2;
-  page.drawText('CUSTOMER DETAILS', {
-    x: 200,
-    y: cursorY,
-    size: 14,
-    font: boldFont,
-    color: rgb(0.725, 0.478, 0.529),
-  });
+  writeText(
+    page,
+    'CUSTOMER DETAILS',
+    200,
+    cursorY,
+    14,
+    boldFont,
+    rgb(0.725, 0.478, 0.529)
+  );
 
   // Customer Name
   cursorY -= lineGap;
-  page.drawText(`Name: `, { x: 60, y: cursorY, size: 12, font });
-  page.drawText(`${customerName}`, {
-    x: 100,
-    y: cursorY,
-    size: 12,
-    font: boldFont,
-    color: rgb(0.725, 0.478, 0.529),
-  });
+  writeText(page, `Name: `, 60, cursorY, 12, font);
+  writeText(
+    page,
+    customerName,
+    100,
+    cursorY,
+    12,
+    boldFont,
+    rgb(0.725, 0.478, 0.529)
+  );
 
   // Customer Email
   cursorY -= lineGap;
-  page.drawText(`Email: `, {
-    x: 60,
-    y: cursorY,
-    size: 12,
-    font,
-  });
-  page.drawText(`${customerEmail}`, {
-    x: 100,
-    y: cursorY,
-    size: 12,
-    font: boldFont,
-    color: rgb(0.725, 0.478, 0.529),
-  });
+  writeText(page, `Email: `, 60, cursorY, 12, font);
+  writeText(
+    page,
+    customerEmail,
+    100,
+    cursorY,
+    12,
+    boldFont,
+    rgb(0.725, 0.478, 0.529)
+  );
 
   // Customer Phone number
   cursorY -= lineGap;
-  page.drawText(`Phone: `, {
-    x: 60,
-    y: cursorY,
-    size: 12,
-    font,
-  });
-  page.drawText(`${customerPhone}`, {
-    x: 100,
-    y: cursorY,
-    size: 12,
-    font: boldFont,
-    color: rgb(0.725, 0.478, 0.529),
-  });
+  writeText(page, `Phone: `, 60, cursorY, 12, font);
+  writeText(
+    page,
+    `${customerPhone}`,
+    100,
+    cursorY,
+    12,
+    boldFont,
+    rgb(0.725, 0.478, 0.529)
+  );
 
   //Customer Address
   cursorY -= lineGap;
-  page.drawText(`Address: `, {
-    x: 60,
-    y: cursorY,
-    size: 12,
-    font,
-  });
-  page.drawText(`${customerAddress}`, {
-    x: 110,
-    y: cursorY,
-    size: 12,
-    font: boldFont,
-    color: rgb(0.725, 0.478, 0.529),
-  });
+  writeText(page, `Address: `, 60, cursorY, 12, font);
+  writeText(
+    page,
+    customerAddress,
+    110,
+    cursorY,
+    12,
+    boldFont,
+    rgb(0.725, 0.478, 0.529)
+  );
 
   cursorY -= lineGap * 2;
-  page.drawLine({
-    start: { x: 50, y: cursorY },
-    end: { x: 550, y: cursorY },
-    thickness: 1,
-    color: rgb(0.725, 0.478, 0.529),
-  });
+  writeLine(
+    page,
+    { x: 50, y: cursorY },
+    { x: 550, y: cursorY },
+    1,
+    rgb(0.725, 0.478, 0.529)
+  );
 
   // === PRODUCT DETAILS ===
   cursorY -= lineGap * 3;
-  page.drawText('TRANSACTION DETAILS', {
-    x: 200,
-    y: cursorY,
-    size: 14,
-    font: boldFont,
-    color: rgb(0.725, 0.478, 0.529),
-  });
+  writeText(
+    page,
+    'TRANSACTION DETAILS',
+    200,
+    cursorY,
+    14,
+    boldFont,
+    rgb(0.725, 0.478, 0.529)
+  );
 
   const unitPrice = product.price;
   const totalPrice = unitPrice * quantity;
 
   // Product Name
   cursorY -= lineGap;
-  page.drawText(`Product Name: `, {
-    x: 80,
-    y: cursorY,
-    size: 12,
-    font,
-  });
-  page.drawText(`${product.name}`, {
-    x: 450,
-    y: cursorY,
-    size: 12,
-    font: boldFont,
-  });
+  writeText(page, `Product Name: `, 80, cursorY, 12, font);
+  writeText(page, product.name, 450, cursorY, 12, boldFont);
 
   // Product Category
   cursorY -= lineGap;
-  page.drawText(`Category: `, {
-    x: 80,
-    y: cursorY,
-    size: 12,
-    font,
-  });
-  page.drawText(`${product.category}`, {
-    x: 450,
-    y: cursorY,
-    size: 12,
-    font: boldFont,
-  });
+  writeText(page, `Category: `, 80, cursorY, 12, font);
+  writeText(page, product.category, 450, cursorY, 12, boldFont);
 
   // Product Unit Price
   cursorY -= lineGap;
-  page.drawText(`Unit Price: `, {
-    x: 80,
-    y: cursorY,
-    size: 12,
-    font,
-  });
-  page.drawText(`$${unitPrice.toFixed(2)}`, {
-    x: 450,
-    y: cursorY,
-    size: 12,
-    font: boldFont,
-  });
+  writeText(page, `Unit Price: `, 80, cursorY, 12, font);
+  writeText(page, `$${unitPrice.toFixed(2)}`, 450, cursorY, 12, boldFont);
 
   // Total Quantity
   cursorY -= lineGap;
-  page.drawText(`Quantity: `, { x: 80, y: cursorY, size: 12, font });
-  page.drawText(`${quantity}`, {
-    x: 450,
-    y: cursorY,
-    size: 12,
-    font: boldFont,
-  });
+  writeText(page, `Quantity: `, 80, cursorY, 12, font);
+  writeText(page, `${quantity}`, 450, cursorY, 12, boldFont);
 
   cursorY -= lineGap;
-  page.drawLine({
-    start: { x: 50, y: cursorY },
-    end: { x: 550, y: cursorY },
-    thickness: 1,
-  });
+  writeLine(page, { x: 50, y: cursorY }, { x: 550, y: cursorY }, 1);
 
   // Total price
   cursorY -= lineGap;
-  page.drawText(`Total Price: `, {
-    x: 80,
-    y: cursorY,
-    size: 12,
-    font: boldFont,
-    color: rgb(0.725, 0.478, 0.529),
-  });
-  page.drawText(`$${totalPrice.toFixed(2)}`, {
-    x: 450,
-    y: cursorY,
-    size: 12,
-    font: boldFont,
-    color: rgb(0.725, 0.478, 0.529),
-  });
+  writeText(
+    page,
+    `Total Price: `,
+    80,
+    cursorY,
+    12,
+    boldFont,
+    rgb(0.725, 0.478, 0.529)
+  );
+  writeText(
+    page,
+    `$${totalPrice.toFixed(2)}`,
+    450,
+    cursorY,
+    12,
+    boldFont,
+    rgb(0.725, 0.478, 0.529)
+  );
 
   cursorY -= lineGap * 2;
 
@@ -260,73 +216,96 @@ const generatePdf = async (transaction) => {
 
   // Manager Name
   cursorY -= lineGap;
-  page.drawText(`Performed By: `, {
-    x: 60,
-    y: cursorY,
-    size: 12,
-    font: boldFont,
-  });
-  page.drawText(`${performedBy?.name || 'N/A'}`, {
-    x: 150,
-    y: cursorY,
-    size: 12,
+  writeText(page, `Performed By: `, 60, cursorY, 12, boldFont);
+  writeText(
+    page,
+    `${performedBy?.name || 'N/A'}`,
+    150,
+    cursorY,
+    12,
     font,
-    color: rgb(0.725, 0.478, 0.529),
-  });
+    rgb(0.725, 0.478, 0.529)
+  );
 
   // Manager Email
   cursorY -= lineGap;
-  page.drawText(`Email: `, {
-    x: 60,
-    y: cursorY,
-    size: 12,
-    font: boldFont,
-  });
-  page.drawText(`${performedBy?.email || 'N/A'}`, {
-    x: 110,
-    y: cursorY,
-    size: 12,
+  writeText(page, `Email: `, 60, cursorY, 12, boldFont);
+  writeText(
+    page,
+    `${performedBy?.email || 'N/A'}`,
+    110,
+    cursorY,
+    12,
     font,
-    color: rgb(0.725, 0.478, 0.529),
-  });
+    rgb(0.725, 0.478, 0.529)
+  );
 
   // Date of Transaction
   cursorY -= lineGap;
-  page.drawText(`Date: `, {
-    x: 60,
-    y: cursorY,
-    size: 12,
-    font: boldFont,
-  });
-  page.drawText(`${formattedDate}`, {
-    x: 110,
-    y: cursorY,
-    size: 12,
+  writeText(page, `Date: `, 60, cursorY, 12, boldFont);
+  writeText(
+    page,
+    `${formattedDate}`,
+    110,
+    cursorY,
+    12,
     font,
-    color: rgb(0.725, 0.478, 0.529),
-  });
+    rgb(0.725, 0.478, 0.529)
+  );
 
-  // === FOOTER ===
+  // ------ FOOTER -------
   cursorY -= lineGap * 3;
-  page.drawLine({
-    start: { x: 50, y: cursorY },
-    end: { x: 550, y: cursorY },
-    thickness: 1,
-    color: rgb(0.725, 0.478, 0.529),
-  });
+  writeLine(
+    page,
+    { x: 50, y: cursorY },
+    { x: 550, y: cursorY },
+    1,
+    rgb(0.725, 0.478, 0.529)
+  );
 
   cursorY -= 20;
-  page.drawText('Thank you for your business!', {
-    x: 200,
-    y: cursorY,
-    size: 12,
+  writeText(
+    page,
+    'Thank you for your business!',
+    200,
+    cursorY,
+    12,
     font,
-    color: rgb(0.725, 0.478, 0.529),
-  });
+    rgb(0.725, 0.478, 0.529)
+  );
 
   // Return PDF bytes
   const pdfBytes = await pdfDoc.save();
   return pdfBytes;
 };
+
+function writeText(page, text, x, y, size, font, color = rgb(0, 0, 0)) {
+  page.drawText(text, {
+    x,
+    y,
+    size,
+    font,
+    color,
+  });
+}
+
+function writeLine(page, start, end, thickness, color) {
+  page.drawLine({
+    start,
+    end,
+    thickness,
+    color,
+  });
+}
+
+function drawImage(page, image, x, y, width, height, opacity = 1) {
+  page.drawImage(image, {
+    x,
+    y,
+    width,
+    height,
+    opacity,
+  });
+}
 
 export default generatePdf;
