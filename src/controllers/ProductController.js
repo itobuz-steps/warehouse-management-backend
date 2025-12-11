9e28902141de752bd5455db6cd3510cd39620f8f
Esha <esha@itobuz.com>
2025-12-11 15:29:35 +0530

    WMDT-162 fix: search and sort in archived products

--- src/controllers/ProductController.js
+++ src/controllers/ProductController.js
@@ -176,16 +176,44 @@ export default class ProductController {
 
   getArchivedProducts = async (req, res, next) => {
     try {
-      const products = await Product.find({ isArchived: true }).populate(
-        'createdBy'
-      );
+      const { search, category, sort } = req.query;
 
+      const filter = { isArchived: true };
+
+      // Category Filter
+      if (category) {
+        filter.category = category;
+      }
+
+      // Search Filter
+      if (search) {
+        filter.name = { $regex: search, $options: 'i' };
+      }
+
+      let query = Product.find(filter).populate('createdBy');
+
+      // Sorting
+      if (sort) {
+        if (sort === 'name_asc') {
+          query = query.sort({ name: 1 });
+        } else if (sort === 'name_desc') {
+          query = query.sort({ name: -1 });
+        } else if (sort === 'quantity_asc') {
+          query = query.sort({ quantity: 1 });
+        } else if (sort === 'quantity_desc') {
+          query = query.sort({ quantity: -1 });
+        } else if (sort === 'category_asc') {
+          query = query.sort({ category: 1 });
+        }
+      }
+
+      const products = await query;
       res.status(200).json({
         success: true,
         data: products,
       });
-    } catch (error) {
-      next(error);
+    } catch (err) {
+      next(err);
     }
   };
 }
